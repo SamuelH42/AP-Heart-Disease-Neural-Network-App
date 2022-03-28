50752bf02542edad52e0e8b80f50d573235afabb
Sam <samuelharris@oldrochester.org>
2022-03-28 17:00:15 -0400

    initial

---
+++ GUI.py
@@ -0,0 +1,130 @@
+# The PySimpleGUI used for formatting and creating the user interface
+import PySimpleGUI as sg
+# My main file with the neural network code
+import main
+# numpy import again
+import numpy as np
+
+question_column = [
+    [
+        sg.Text("Health Questions: "
+                "\n\n2: Do you have high blood pressure?"
+                "\n\n3: Do you have high cholesterol"
+                "\n\n4: Do you smoke often?"
+                "\n\n5: Have you ever had a stroke?"
+                "\n\n6: Do you have diabetes?"
+                "\n\n7: Do you exercise often?"
+                "\n\n8: Do you eat fruits often?"
+                "\n\n9: Do you eat veggies often?"
+                "\n\n10: Do you have regular check ups?")
+
+    ]
+]
+
+yes_no_column = [
+    [sg.Button("Start AI", enable_events=True)],
+    [sg.Text("Answers: ")],
+    [sg.Text("2: ")], [sg.Button("Yes(2)", enable_events=True), sg.Button("No(2)", enable_events=True)],
+    [sg.Text("3: ")], [sg.Button("Yes(3)", enable_events=True), sg.Button("No(3)", enable_events=True)],
+    [sg.Text("4: ")], [sg.Button("Yes(4)", enable_events=True), sg.Button("No(4)", enable_events=True)],
+    [sg.Text("5: ")], [sg.Button("Yes(5)", enable_events=True), sg.Button("No(5)", enable_events=True)],
+    [sg.Text("6: ")], [sg.Button("Yes(6)", enable_events=True), sg.Button("No(6)", enable_events=True)],
+    [sg.Text("7: ")], [sg.Button("Yes(7)", enable_events=True), sg.Button("No(7)", enable_events=True)],
+    [sg.Text("8: ")], [sg.Button("Yes(8)", enable_events=True), sg.Button("No(8)", enable_events=True)],
+    [sg.Text("9: ")], [sg.Button("Yes(9)", enable_events=True), sg.Button("No(9)", enable_events=True)],
+    [sg.Text("10: ")], [sg.Button("Yes(10)", enable_events=True), sg.Button("No(10)", enable_events=True)],
+    [sg.Text("11: ")], [sg.Button("Yes(11)", enable_events=True), sg.Button("No(11)", enable_events=True)],
+    [sg.Text("12: ")], [sg.Button("Yes(12)", enable_events=True), sg.Button("No(12)", enable_events=True)],
+    [sg.Button("Submit Answers", enable_events=True)]
+]
+
+layout = [
+    [
+        sg.Column(question_column),
+        sg.VSeperator(),
+        sg.Column(yes_no_column)
+    ]
+]
+
+GUI_input = []
+past_trained_weights = []
+
+
+def add_answer(button_number, yes_no):
+    if yes_no == "Yes":
+        GUI_input.insert(button_number - 2, 1)
+    else:
+        GUI_input.insert(button_number - 2, 0)
+
+
+window1 = sg.Window("AI Health App", layout)
+
+while True:
+    event, values = window1.read()
+    if event == sg.WIN_CLOSED:
+        break
+
+    if event == "Start AI":
+        main.neural_network = main.ANN()
+
+        print("Random Weights: ")
+        print(main.neural_network.synaptic_weights)
+
+        training_inputs = main.train_inputs
+        training_outputs = main.train_outputs
+
+        main.neural_network.train(training_inputs, training_outputs, 10000)
+
+        print('Weight after training: ')
+        print(main.neural_network.synaptic_weights)
+        past_trained_weights.append(main.neural_network.synaptic_weights)
+
+    if event == "Yes(2)":
+        add_answer(1, "Yes")
+    elif event == "No(2)":
+        add_answer(1, "No")
+    elif event == "Yes(3)":
+        add_answer(2, "Yes")
+    elif event == "No(3)":
+        add_answer(2, "No")
+    elif event == "Yes(4)":
+        add_answer(4, "Yes")
+    elif event == "No(4)":
+        add_answer(4, "No")
+    elif event == "Yes(5)":
+        add_answer(5, "Yes")
+    elif event == "No(5)":
+        add_answer(5, "No")
+    elif event == "Yes(6)":
+        add_answer(6, "Yes")
+    elif event == "No(6)":
+        add_answer(6, "No")
+    elif event == "Yes(7)":
+        add_answer(7, "Yes")
+    elif event == "No(7)":
+        add_answer(7, "No")
+    elif event == "Yes(8)":
+        add_answer(8, "Yes")
+    elif event == "No(8)":
+        add_answer(8, "No")
+    elif event == "Yes(9)":
+        add_answer(9, "Yes")
+    elif event == "No(9)":
+        add_answer(9, "No")
+    elif event == "Yes(10)":
+        add_answer(10, "Yes")
+    elif event == "No(10)":
+        add_answer(10, "No")
+    elif event == "Yes(11)":
+        add_answer(11, "Yes")
+    elif event == "No(11)":
+        add_answer(11, "No")
+    elif event == "Yes(12)":
+        add_answer(12, "Yes")
+    elif event == "No(12)":
+        add_answer(12, "No")
+
+    if event == "Submit Answers":
+        layout2 = [[sg.Text(str(main.neural_network.think(np.array([GUI_input]))))]]
+        window2 = sg.Window("AI App Results", layout2)
+        event, values = window2.read()
---
+++ main.py
@@ -0,0 +1,67 @@
+# Credit to https://www.kdnuggets.com/2018/10/simple-neural-network-python.html for information on creating ANN class
+# Credit to https://www.youtube.com/watch?v=z1PGJ9quPV8&list=PL3t2bW_hFraVDOxMPH4lWCUerCqG-fIJV&index=1&t=753s for information on working with pandas
+
+# numpy library used for math functions
+import numpy as np
+# pandas used for data set reading and writing
+import pandas as pd
+
+dataset = pd.read_csv("heart_disease_health_indicators_BRFSS2015.csv")
+train_inputs = pd.DataFrame.drop(dataset, columns=['HeartDiseaseorAttack', 'NoDocbcCost', 'GenHlth', 'MentHlth', 'PhysHlth', 'DiffWalk', 'Sex', 'Age', 'Education', 'Income', "BMI"])
+train_outputs = pd.DataFrame(dataset, columns=['HeartDiseaseorAttack'])
+
+
+user_choices = []
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def sigmoid_derivative(x):
+    return x * (1 - x)
+
+
+def user_input(iterations):
+    for i in range(iterations):
+        user_choices.append((input("User Input" + str(i))))
+
+
+class ANN():
+    def __init__(self):
+        np.random.seed(1)
+        self.synaptic_weights = 2 * np.random.random((11, 1)) - 1
+
+    def train(self, training_inputs, training_outputs, training_iterations):
+        for iteration in range(training_iterations):
+            output = self.think(training_inputs)
+            error = training_outputs - output
+            adjustments = np.dot(training_inputs.T, error * sigmoid_derivative(output))
+            self.synaptic_weights += adjustments
+
+    def think(self, inputs):
+        inputs = inputs.astype(float)
+        output = sigmoid(np.dot(inputs, self.synaptic_weights))
+        return output
+
+
+if __name__ == '__main__':
+    neural_network = ANN()
+
+    print("Random Weights: ")
+    print(neural_network.synaptic_weights)
+
+    training_inputs = train_inputs
+    training_outputs = train_outputs
+
+    neural_network.train(training_inputs, training_outputs, 10000)
+
+    print('Weight after training: ')
+    print(neural_network.synaptic_weights)
+
+    user_input(11)
+
+    print("Considering New Situation: " + str(user_choices))
+    print('Output: ')
+    print(neural_network.think(np.array([user_choices])))
+
